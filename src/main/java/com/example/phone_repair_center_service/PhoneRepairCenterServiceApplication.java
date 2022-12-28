5bb7541e3fac5821ee6c3783f5a280375e9d2448
libre-alex <libre_maiden@protonmail.com>
2022-12-28 16:13:35 +0200

    first commit

---
+++ src/main/java/com/example/phone_repair_center_service/PhoneRepairCenterServiceApplication.java
@@ -0,0 +1,13 @@
+package com.example.phone_repair_center_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PhoneRepairCenterServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PhoneRepairCenterServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/phone_repair_center_service/PhoneRepairCenterServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.phone_repair_center_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PhoneRepairCenterServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
